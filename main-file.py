5805a6569aa4e119a98618a0111d900350b15846
Acrasy <sai.whocares@gmail.com>
2016-07-09 09:41:10 +0200

    branch

--- main-file.py
+++ main-file.py
@@ -5,10 +5,10 @@ with open('mein_dp.csv',encoding='latin-1') as infile:# ,open("clean_mein_dp.csv
     data = csv.reader(infile)#, delimiter=',')
     newfile = 'clean_mein_dp.csv'                            # define name for the new file, open later when append
 #next(data, None)                                            #skips first row
-    rmv = {'Microsoft Promotion 2016' , 'Mariahilfer Straße 42-48-1070-Wien' , 'Sales Promotion', 'Microsoft Promotion 2016' , 'Inaktiv'}                                                   #helper,what to remove
-    for row in data:                                        # going through rows
-        new_csv  = []                                       #new list we are going to write to
+    new_csv  = [][]
+    for row in data:
         for column in row:                                  #going through columns
-            if column not in rmv:                           #filter keywords
-                new_csv.append(column)                      #append to list if not keyword
-        csv.writer(open(newfile,'a')).writerow(new_csv)     #write row (open file,append)
+            new_csv.append(column)                      #append to list if not keyword
+
+    for i in range(length(new_csv)):
+        csv.writer(open(newfile,'a')).writerow(new_csv[i])    
